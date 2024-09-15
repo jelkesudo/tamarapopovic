36e966999021ddaf7e3ec4624bf4c3660e1edbba
jelkesudo <ficajelic@gmail.com>
2024-09-15 19:00:28 +0200

    nav bar change

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,33 @@
 window.onload = function(){
+    
+    $("#hamburger-menu").on('click', (e) => {
+        e.stopPropagation();
+        $("#nav-menu").addClass("active");
+        $("#overlay").addClass("active");
+    });
+
+    $("#close-nav").on('click', (e) => {
+        $("#nav-menu").removeClass("active");
+        $("#overlay").removeClass("active");
+    });
+
+    $("#overlay").on('click', () => {
+        nav.classList.remove('active');
+        overlay.classList.remove('active');
+    });
+
+    const hamburger = document.getElementById('hamburger-menu');
+    const nav = document.getElementById('nav-menu');
+
+    document.addEventListener('click', (event) => {
+        const isClickInsideNav = nav.contains(event.target);
+        const isClickOnHamburger = hamburger.contains(event.target);
+    
+        if (!isClickInsideNav && !isClickOnHamburger) {
+            $("#nav-menu").removeClass("active");
+            $("#overlay").removeClass("active");
+        }
+    });
     getImages();
 }
 
