51961c7ab28d2f32087dd07dee22589a0819bf90
jelkesudo <ficajelic@gmail.com>
2024-11-15 15:54:39 +0100

    scroll fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,108 +38,34 @@ function getImages(){
 }
 
 function printImages(data){
-    let html, dotHtml = "";
-    let first = true;
+    let html= "";
+    let dotHtml = "";
 
     for (let index = 0; index < data.length; index++) {
         const d = data[index];
-        console.log(d);
-        html += `<div class="carousel-item active">
-                        <div class="carousel-image">
-                            <img src="${d.src}" alt="${d.alt}"/>
-                        </div>
-                        <div class="carousel-overlay">
-                            <div class="carousel-content">
-                                <div class="carousel-heading">
-                                    <h3>Ime projekta</h3>
+        html += `<div class="carousel-item ${index === 0 ? "active" : ""}" data-bs-interval="${index === 0 ? "5000" : "2000"}">
+                            <img src="assets/img/nas-razred/14-tamara-popovic-pozoriste-67362e62d418e.webp"
+                                class="d-block w-100" alt="...">
+                            <div class="carousel-caption">
+                                <h5>Ime Projekta ${index + 1}</h5>
+                                <div class="d-none d-md-block">
+                                    <p>Some representative placeholder content for the second slide.</p>
                                 </div>
-                                <div class="carousel-text">
-                                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, ipsum deleniti repudiandae a molestias fugit consequatur, saepe consequuntur nulla laudantium commodi repellat praesentium asperiores aut atque, accusamus autem harum dicta.</p>
-                                </div>
-                                <div class="carousel-button">
-                                    <button>Go to project</button>
-                                </div>
+                                <button class="btn btn-primary">Go to project</button>
                             </div>
-                        </div>
-                    </div>`;
-
-        dotHtml += `<span class="dot" data-index="${index}"></span>`;
-        if(first){
-            html = `<div class="carousel-image active">
-                        <img src="${d.src}" alt="${d.alt}"/>
-                    </div>`;
-
-            dotHtml = `<span class="dot active" data-index="${index}"></span>`;
-            first = false;
-        }
+                        </div>`;
     }
+    html += `<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark"
+                        data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark"
+                        data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>`
     $("#imagePrint").html(html);
-    $("#dotPrint").html(dotHtml);
-    const carouselImages = document.querySelector('.carousel-images');
-    const images = document.querySelectorAll('.carousel-image');
-    const dots = document.querySelectorAll('.dot');
-    const leftArrow = document.querySelector('#left-arrow');
-    const rightArrow = document.querySelector('#right-arrow');
-    let currentIndex = 0;
-
-    function updateCarousel() {
-        const offset = -currentIndex * 100;
-        carouselImages.style.transform = `translateX(${offset}%)`;
-
-        images.forEach((img, index) => {
-            img.classList.remove('active');
-            if (index === currentIndex) {
-                img.classList.add('active');
-            }
-        });
-        dots.forEach((img, index) => {
-            img.classList.remove('active');
-            if (index === currentIndex) {
-                img.classList.add('active');
-            }
-        });
-    }
-
-    function showNextImage() {
-        currentIndex = (currentIndex + 1) % images.length;
-        updateCarousel();
-    }
-
-    function showPreviousImage() {
-        currentIndex = (currentIndex - 1 + images.length) % images.length;
-        updateCarousel();
-    }
-
-    rightArrow.addEventListener('click', showNextImage);
-    leftArrow.addEventListener('click', showPreviousImage);
-
-    let startX, endX;
-
-    carouselImages.addEventListener('mousedown', (event) => {
-        startX = event.pageX;
-    });
-
-    carouselImages.addEventListener('mouseup', (event) => {
-        endX = event.pageX;
-        if (startX > endX + 50) {
-            showNextImage();
-        } else if (startX + 50 < endX) {
-            showPreviousImage();
-        }
-    });
-
-    carouselImages.addEventListener('touchstart', (event) => {
-        startX = event.touches[0].pageX;
-    });
-
-    carouselImages.addEventListener('touchend', (event) => {
-        endX = event.changedTouches[0].pageX;
-        if (startX > endX + 50) {
-            showNextImage();
-        } else if (startX + 50 < endX) {
-            showPreviousImage();
-        }
-    });
 }
 
 function ajaxCallBack(address, f){
